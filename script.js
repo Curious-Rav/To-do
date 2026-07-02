9b7346f7e9d0eb8d67b8679ee5146b1ec609e87c
Abhirav Arsh <abhiravshresth10@gmail.com>
2026-07-02 17:56:31 +0530

    included trash button

--- script.js
+++ script.js
@@ -5,22 +5,38 @@ let todoinput=document.querySelector("#todo-input")
 let turn=0
 add.addEventListener("click",(evt)=>{
     if(todoinput.value!==""){
-    let newitem=document.createElement("li")
-    newitem.innerText=todoinput.value
-    newitem.classList.add("todo-item")
-    newitem.addEventListener("dblclick",()=>{
-        if(turn=="0"){
-            newitem.classList.remove("todo-item")
-            newitem.classList.add("completed")
-            turn=1
-        }else{
-            newitem.classList.add("todo-item")
-            newitem.classList.remove("completed")
-            turn=0
-        }
-    })
-    todoinput.value=""
-    list.prepend(newitem)} 
+        let newitem=document.createElement("li")
+        newitem.classList.add("todo-item")
+
+        // 1. Create a span for the text to separate it from the button
+        let textSpan = document.createElement("span")
+        textSpan.innerText = todoinput.value
+        newitem.appendChild(textSpan) // Put text inside li
+
+        // 2. Create the delete button
+        let newdel=document.createElement("button")
+        newdel.classList.add("delete-btn")
+        newdel.innerHTML=`<i class="fa-solid fa-trash" aria-hidden="true"></i>`
+        
+        // 3. CRITICAL: Append the delete button inside the li element!
+        newitem.appendChild(newdel) 
+
+        // Your existing dblclick listener
+        newitem.addEventListener("dblclick",()=>{
+            if(turn=="0"){
+                newitem.classList.remove("todo-item")
+                newitem.classList.add("completed")
+                turn=1
+            }else{
+                newitem.classList.add("todo-item")
+                newitem.classList.remove("completed")
+                turn=0
+            }
+        })
+
+        todoinput.value=""
+        list.prepend(newitem)
+    } 
 })
 
 
